0de2efc25e4480d1a792b3529121ddeb635ee14c
smuss96 <giordanosimone96@gmail.com>
2025-01-03 01:26:05 +0100

    Cambiato server.js

--- server.js
+++ server.js
@@ -19,56 +19,61 @@ console.log('Certificati SSL letti correttamente');
 // Creare il server HTTPS per servire il file HTML
 const server = https.createServer(credentials, (req, res) => {
     console.log('Ricevuta richiesta per', req.url);
-    const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
+    if (req.url === '/playAudio' || req.url === '/pauseAudio' || req.url === '/playVideo' || req.url === '/pauseVideo') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`Received request for ${req.url}`);
+    } else {
+        const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+        const extname = path.extname(filePath);
+        let contentType = 'text/html';
 
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.gif':
-            contentType = 'image/gif';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-        case '.mp4':
-            contentType = 'video/mp4';
-            break;
-        case '.mp3':
-            contentType = 'audio/mpeg';
-            break;
-    }
-
-    fs.readFile(filePath, (error, content) => {
-        if (error) {
-            if (error.code === 'ENOENT') {
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.end(content, 'utf8');
-                });
-            } else {
-                res.writeHead(500);
-                res.end(`Server Error: ${error.code}`);
-            }
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf8');
+        switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.gif':
+                contentType = 'image/gif';
+                break;
+            case '.wav':
+                contentType = 'audio/wav';
+                break;
+            case '.mp4':
+                contentType = 'video/mp4';
+                break;
+            case '.mp3':
+                contentType = 'audio/mpeg';
+                break;
         }
-    });
+
+        fs.readFile(filePath, (error, content) => {
+            if (error) {
+                if (error.code === 'ENOENT') {
+                    fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(content, 'utf8');
+                    });
+                } else {
+                    res.writeHead(500);
+                    res.end(`Server Error: ${error.code}`);
+                }
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf8');
+            }
+        });
+    }
 });
 
 // Creare il server WebSocket
